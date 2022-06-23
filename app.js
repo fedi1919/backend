4465c2517bb644be0aeadba2c46761ee165599c1
fedi1919 <fedi.hammami.stic@gmail.com>
2022-06-23 16:24:38 +0200

    Adding our own error model

--- app.js
+++ app.js
@@ -7,4 +7,14 @@ const app = express();
 
 app.use("/api/places", placesRoutes);
 
+app.use((error, req, res, next) => {
+  // check if a response has already been sent
+  if (res.headerSent) {
+    return next(error);
+  }
+  //Sendig a reponse
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknon error occured!" });
+});
 app.listen(5000);
